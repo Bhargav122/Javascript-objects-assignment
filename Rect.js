49ec5ae180f4574a0e2d38d6e98a443911d20b86
bhargav <93025658+Bhargav122@users.noreply.github.com>
2022-05-16 13:14:55 +0530

    Update Rect.js

--- Rect.js
+++ Rect.js
@@ -1,6 +1,7 @@
+//declaring object with constructor function
 var Rectangle = function (
   width,
-  height //declaring object with "constructor function"
+  height 
 ) {
   this.width = width;
   this.height = height;
@@ -8,17 +9,18 @@ var Rectangle = function (
     console.log("Area of the Rectangle is : " + this.width * this.height);
   };
 };
-
-var myRectangle1 = new Rectangle(40, 10); //calling objects/making intances
+ //calling objects/making intances
+var myRectangle1 = new Rectangle(40, 10);
 var myRectangle2 = new Rectangle(50, 10);
 
 
-
-console.log(myRectangle1.width); //calling objects' properties
+//calling objects properties
+console.log(myRectangle1.width); 
 console.log(myRectangle1.height);
 myRectangle1.area();
-console.log("---------------------");
-console.log(myRectangle2.width); //calling objects' properties
+console.log(":::::::::::::::::::");
+//calling objects properties
+console.log(myRectangle2.width); 
 console.log(myRectangle2.height);
 myRectangle2.area();
 
