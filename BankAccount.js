03f8f5b01befbada8fc84fc9867f1dfe6563e833
bhargav <93025658+Bhargav122@users.noreply.github.com>
2022-05-17 20:30:00 +0530

    Update BankAccount.js

--- BankAccount.js
+++ BankAccount.js
@@ -10,34 +10,13 @@ function Savings(isSalary, accountNumber, accountHolderName, accountBalance) {
   this.accountBalance = accountBalance;
   this.withdraw = function (amount) {
     if (this.accountBalance > 0 && this.accountBalance < 100) {
-      console.log("Insuffient balance! You have less than 100 rs.");
+      console.log("Balance is less than 100 rs can't withdraw.");
     }
-    if (
-      this.accountBalance >= 100 &&
-      this.accountBalance <= 500 &&
-      amount === 200
-    ) {
+    if (this.accountBalance >= 100) {
       this.accountBalance = this.accountBalance - amount;
       console.log("Your money after debiting : " + this.accountBalance);
     }
-    if (
-      (this.accountBalance > 500 &&
-        this.accountBalance <= 5000 &&
-        amount === 500) ||
-      amount === 2000
-    );
-    {
-      this.accountBalance = this.accountBalance - amount;
-      console.log("Your money after debiting : " + this.accountBalance);
-    }
-    if (
-      (this.accountBalance > 5000 && amount === 200) ||
-      amount === 500 ||
-      amount === 2000
-    ) {
-      this.accountBalance = this.accountBalance - amount;
-      console.log("Your money after debiting : " + this.accountBalance);
-    }
+
     if (this.accountBalance <= 0) {
       console.log("you had zero balance");
     }
@@ -53,24 +32,11 @@ function Current(odLimit, accountNumber, accountHolderName, accountBalance) {
   this.accountBalance = accountBalance;
   this.withdraw = function (amount) {
     if (odLimit > 500) {
-      if (
-        (this.accountBalance > 100 &&
-          this.accountBalance < 5000000 &&
-          amount === 200) ||
-        amount === 100
-      ) {
+      if (this.accountBalance > 100) {
         this.accountBalance = this.accountBalance - amount;
         console.log(
           "Your balance after debiting the money is : " + this.accountBalance
         );
-      }
-      if (
-        (this.accountBalance > 2000 && amount === 200) ||
-        amount === 500 ||
-        amount === 2000
-      ) {
-        this.accountBalance = this.accountBalance - amount;
-        console.log("Balance after debit: " + this.accountBalance);
       }
     } else {
       console.log("Your odLimit for Current acocunt is less than 500");
@@ -86,10 +52,10 @@ console.log(obj1);
 // this is savings object
 var objSavings = new Savings("true", 102, "Raju", 80000);
 console.log(objSavings);
-console.log(objSavings.withdraw(1500));
+console.log(objSavings.withdraw(300));
 console.log(objSavings.getCurrentBalance());
 //this is Current object
-var objCurrent = new Current(550, 101, "Santosh", 50050);
+var objCurrent = new Current(600, 101, "Santosh", 50050);
 console.log(objCurrent);
 console.log(objCurrent.withdraw(200));
 console.log(objCurrent.getCurrentBalance());
